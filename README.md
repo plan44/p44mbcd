f79a2f62b55d8fd29925f3aa7ac680c9c39c36f3
Lukas Zeller <luz@plan44.ch>
2019-04-26 10:53:01 +0200

    Initial commit, dummy daemon doing nothing

---
+++ README.md
@@ -0,0 +1,21 @@
+
+p44mbcd
+=======
+
+"p44mbcd" is a free (opensource, GPLv3) daemon for a modbus touch lcd control unit
+
+p44mbcd is based on a set of generic C++ utility classes called [**p44utils**](https://github.com/plan44/p44utils), which provides basic mechanisms for mainloop-based, nonblocking I/O driven automation daemons. p44utils is also included as a submodule into this project.
+
+License
+-------
+
+p44mbcd is licensed under the GPLv3 License (see COPYING).
+
+*(c) 2019 by Lukas Zeller / [plan44.ch](http://www.plan44.ch/automation)*
+
+
+
+
+
+
+
---
+++ src/main.cpp
@@ -0,0 +1,91 @@
+//
+//  main.cpp
+//  p44mbc
+//
+//  Created by Lukas Zeller on 2019-04-26
+//  Copyright (c) 2019 plan44.ch. All rights reserved.
+//
+
+#include "application.hpp"
+
+#include "macaddress.hpp"
+#include "digitalio.hpp"
+#include "utils.hpp"
+
+#include <stdio.h>
+
+#define DEFAULT_LOGLEVEL LOG_NOTICE
+
+using namespace p44;
+
+
+class P44mbcd : public CmdLineApp
+{
+  typedef CmdLineApp inherited;
+
+public:
+
+  P44mbcd()
+  {
+  }
+
+
+  virtual int main(int argc, char **argv)
+  {
+    const char *usageText =
+    "Usage: %1$s [options]\n";
+    const CmdLineOptionDescriptor options[] = {
+      { 0  , "ubusapi",         false, "enable ubus api" },
+      { 'l', "loglevel",        true,  "level;set max level of log message detail to show on stderr" },
+      { 0  , "deltatstamps",    false, "show timestamp delta between log lines" },
+      { 'V', "version",         false, "show version" },
+      { 'h', "help",            false, "show this text" },
+      { 0, NULL } // list terminator
+    };
+
+    // parse the command line, exits when syntax errors occur
+    setCommandDescriptors(usageText, options);
+    parseCommandLine(argc, argv);
+
+    if (numOptions()<1) {
+      // show usage
+      showUsage();
+      terminateApp(EXIT_SUCCESS);
+    }
+
+    // log level?
+    int loglevel = DEFAULT_LOGLEVEL;
+    getIntOption("loglevel", loglevel);
+    SETLOGLEVEL(loglevel);
+    SETERRLEVEL(loglevel, false); // all diagnostics go to stderr
+    SETDELTATIME(getOption("deltatstamps"));
+
+    // app now ready to run
+    return run();
+  }
+
+
+  // MARK: ===== command line actions
+
+  virtual void initialize()
+  {
+    LOG(LOG_NOTICE, "p44mbcd. initialize");
+  }
+
+};
+
+
+int main(int argc, char **argv)
+{
+  // prevent all logging until command line determines level
+  SETLOGLEVEL(LOG_EMERG);
+  SETERRLEVEL(LOG_EMERG, false); // messages, if any, go to stderr
+  // create app with current mainloop
+  P44mbcd *application = new(P44mbcd);
+  // pass control
+  int status = application->main(argc, argv);
+  // done
+  delete application;
+  return status;
+}
+
