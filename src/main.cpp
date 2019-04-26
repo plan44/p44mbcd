fea94b1bcad32a3a2e3456c335ff973eba49025f
Lukas Zeller <luz@plan44.ch>
2019-04-26 09:38:31 +0000

    add ubus api (for loglevel + deltastamps only, for now)

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,10 @@
 #include "digitalio.hpp"
 #include "utils.hpp"
 
+#if ENABLE_UBUS
+  #include "ubus.hpp"
+#endif
+
 #include <stdio.h>
 
 #define DEFAULT_LOGLEVEL LOG_NOTICE
@@ -19,16 +23,33 @@
 using namespace p44;
 
 
+#if ENABLE_UBUS
+static const struct blobmsg_policy logapi_policy[] = {
+  { .name = "level", .type = BLOBMSG_TYPE_INT8 },
+  { .name = "deltastamps", .type = BLOBMSG_TYPE_BOOL },
+  { .name = NULL, .type = BLOBMSG_TYPE_INT32 },
+};
+
+static const struct blobmsg_policy p44mbcapi_policy[] = {
+  { .name = "method", .type = BLOBMSG_TYPE_STRING },
+  { .name = NULL, .type = BLOBMSG_TYPE_UNSPEC },
+};
+#endif
+
+
 class P44mbcd : public CmdLineApp
 {
   typedef CmdLineApp inherited;
+
+  #if ENABLE_UBUS
+  UbusServerPtr ubusApiServer; ///< ubus API for openwrt web interface
+  #endif
 
 public:
 
   P44mbcd()
   {
   }
-
 
   virtual int main(int argc, char **argv)
   {
@@ -39,7 +60,9 @@ public:
       { 0  , "rs485txenable",   true,  "pinspec;a digital output pin specification for TX driver enable or DTR or RTS" },
       { 0  , "rs485txoffdelay", true,  "delay;time to keep tx enabled after sending [ms], defaults to 0" },
       { 0  , "rs485rxenable",   true,  "pinspec;a digital output pin specification for RX driver enable" },
-      { 0  , "ubusapi",         false, "enable ubus api" },
+      #if ENABLE_UBUS
+      { 0  , "ubusapi",         false, "enable ubus API" },
+      #endif
       { 'l', "loglevel",        true,  "level;set max level of log message detail to show on stderr" },
       { 0  , "deltatstamps",    false, "show timestamp delta between log lines" },
       { 'V', "version",         false, "show version" },
@@ -64,16 +87,70 @@ public:
     SETERRLEVEL(loglevel, false); // all diagnostics go to stderr
     SETDELTATIME(getOption("deltatstamps"));
 
+    #if ENABLE_UBUS
+    // Prepare ubus API
+    if (getOption("ubusapi")) {
+      initUbusApi();
+    }
+    #endif
+
     // app now ready to run
     return run();
   }
 
 
-  // MARK: ===== command line actions
+  // MARK: ===== ubus API
+
+  #if ENABLE_UBUS
+
+  void initUbusApi()
+  {
+    ubusApiServer = UbusServerPtr(new UbusServer(MainLoop::currentMainLoop()));
+    UbusObjectPtr u = new UbusObject("p44mbcd", boost::bind(&P44mbcd::ubusApiRequestHandler, this, _1, _2, _3));
+    u->addMethod("log", logapi_policy);
+    u->addMethod("api", p44mbcapi_policy);
+    ubusApiServer->registerObject(u);
+  }
+
+  void ubusApiRequestHandler(UbusRequestPtr aUbusRequest, const string aMethod, JsonObjectPtr aJsonRequest)
+  {
+    if (aMethod=="log") {
+      if (aJsonRequest) {
+        JsonObjectPtr o;
+        if (aJsonRequest->get("loglevel", o)) {
+          int oldLevel = LOGLEVEL;
+          int newLevel = o->int32Value();
+          SETLOGLEVEL(newLevel);
+          LOG(newLevel, "\n\n========== changed log level from %d to %d ===============", oldLevel, newLevel);
+        }
+        if (aJsonRequest->get("deltastamps", o)) {
+          SETDELTATIME(o->boolValue());
+        }
+      }
+      aUbusRequest->sendResponse(JsonObjectPtr());
+    }
+    else if (aMethod=="api") {
+      aUbusRequest->sendResponse(JsonObjectPtr(), UBUS_STATUS_INVALID_COMMAND);
+    }
+    else {
+      // no other methods implemented yet
+      aUbusRequest->sendResponse(JsonObjectPtr(), UBUS_STATUS_INVALID_COMMAND);
+    }
+  }
+
+  #endif
+
+  // MARK: ===== initialisation
 
   virtual void initialize()
   {
-    LOG(LOG_NOTICE, "p44mbcd. initialize");
+    LOG(LOG_NOTICE, "p44mbcd: initialize");
+    #if ENABLE_UBUS
+    // start ubus API, if we have it
+    if (ubusApiServer) {
+      ubusApiServer->startServer();
+    }
+    #endif
   }
 
 };
