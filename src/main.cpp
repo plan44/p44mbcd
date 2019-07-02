fa241deac547ca0175924a4b475bcdd83acf4d83
Lukas Zeller <luz@plan44.ch>
2019-07-02 23:06:36 +0000

    adapt main program to use new p44util's modbus.hpp/.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,7 @@
 #include "application.hpp"
 
 #include "macaddress.hpp"
-#include "digitalio.hpp"
-#include "serialcomm.hpp"
+#include "modbus.hpp"
 #include "utils.hpp"
 
 #if ENABLE_UBUS
@@ -26,15 +25,11 @@
   // - little vGL
   #include "lv_drivers/display/monitor.h"
   #include "lv_drivers/indev/mouse.h"
-  // - modbus
-  #include "modbus-rtu.h"
 #else
   // target platform build
   // - little vGL
   #include "lv_drivers/display/fbdev.h"
   #include "lv_drivers/indev/evdev.h"
-  // - modbus
-  #include <modbus/modbus-rtu.h>
 #endif
 
 // FIXME: remove later
@@ -64,26 +59,9 @@ static const struct blobmsg_policy p44mbcapi_policy[] = {
 #endif
 
 
-class ModBusError : public Error
-{
-public:
-  static const char *domain() { return "Modbus"; }
-  virtual const char *getErrorDomain() const { return ModBusError::domain(); };
-  ModBusError(ErrorCode aError) : Error(aError, modbus_strerror((int)aError)) {};
-};
-
-
-extern "C" {
-  void setRts(modbus_t *ctx, int on);
-}
-
-
 class P44mbcd : public CmdLineApp
 {
   typedef CmdLineApp inherited;
-
-  friend void setRts(modbus_t *ctx, int on);
-
 
   #if ENABLE_UBUS
   UbusServerPtr ubusApiServer; ///< ubus API for openwrt web interface
@@ -101,9 +79,7 @@ class P44mbcd : public CmdLineApp
   bool showCursor = false;
 
   // modbus
-  int slave = 1;
-  modbus_t *modbus;
-  DigitalIoPtr modbusRTSEnable;
+  ModbusMaster modBus;
 
   // app
   MLTicket appTicket;
@@ -117,6 +93,7 @@ public:
     keyboard_indev(NULL),
     lastLvglTick(Never)
   {
+    modBus.isMemberVariable();
   }
 
   virtual int main(int argc, char **argv)
@@ -125,9 +102,8 @@ public:
     "Usage: %1$s [options]\n";
     const CmdLineOptionDescriptor options[] = {
       { 0  , "connection",      true,  "connspec;serial interface for RTU or IP address for TCP (/device or IP[:port])" },
-      { 0  , "rs485txenable",   true,  "pinspec;a digital output pin specification for TX driver enable or DTR or RTS" },
-      { 0  , "rs485rtsdelay",   true,  "delay;delay of tx enable signal (RTS) in uS" },
-      { 0  , "rs232",           false, "use RS-232 for RTU" },
+      { 0  , "rs485txenable",   true,  "pinspec;a digital output pin specification for TX driver enable, 'RTS' or 'RS232'" },
+      { 0  , "rs485txdelay",    true,  "delay;delay of tx enable signal in uS" },
       { 0  , "slave",           true,  "slave;use this slave by default" },
       { 0  , "mousecursor",     false, "show mouse cursor" },
       { 0  , "testmode",        false, "FCU test mode" },
@@ -337,152 +313,30 @@ public:
     }
     #endif
     // init modbus
-    initModbus();
+    string mbconn;
+    if (!getStringOption("connection", mbconn)) {
+      terminateAppWith(TextError::err("must specify --connection"));
+      return;
+    }
+    string txen;
+    getStringOption("rs485txenable", txen);
+    int txDelayUs = Never;
+    getIntOption("rs485txdelay", txDelayUs);
+    ErrorPtr err = modBus.setConnectionSpecification(mbconn.c_str(), DEFAULT_MODBUS_IP_PORT, DEFAULT_MODBUS_RTU_PARAMS, txen.c_str(), txDelayUs);
+    if (!Error::isOK(err)) {
+      terminateAppWith(err->withPrefix("Invalid modbus connection: "));
+      return;
+    }
+    int slave;
+    if (getIntOption("slave", slave)) {
+      modBus.setSlaveAddress(slave);
+    }
     // start littlevGL
     initLvgl();
     // start app
     if (!isTerminated())
       initApp();
   }
-
-
-  // MARK: - modbus
-
-
-  void initModbus()
-  {
-    LOG(LOG_NOTICE, "initializing modbus");
-    // init modbus library
-    string rs485conn;
-    if (!getStringOption("connection", rs485conn)) {
-      terminateAppWith(TextError::err("must specify --connection"));
-      return;
-    }
-    string connectionPath;
-    uint16_t connectionPort;
-    int baudRate;
-    int charSize; // character size 5..8 bits
-    bool parityEnable;
-    bool evenParity;
-    bool twoStopBits;
-    bool hardwareHandshake;
-    bool rtu = SerialComm::parseConnectionSpecification(
-      rs485conn.c_str(), DEFAULT_MODBUS_IP_PORT, DEFAULT_MODBUS_RTU_PARAMS,
-      connectionPath,
-      baudRate,
-      charSize,
-      parityEnable,
-      evenParity,
-      twoStopBits,
-      hardwareHandshake,
-      connectionPort
-    );
-    int mberr = 0;
-    if (rtu) {
-      bool rs232 = getOption("rs232")!=NULL;
-      if (!rs232) {
-        string rtsEn;
-        if (getStringOption("rs485txenable", rtsEn)) {
-          modbusRTSEnable = DigitalIoPtr(new DigitalIo(rtsEn.c_str(), true, false));
-        }
-        else {
-          LOG(LOG_WARNING, "no --rs485txenable specified, RTS of serial port must exist");
-        }
-      }
-      if (baudRate==0 || connectionPath.empty()) {
-        terminateAppWith(TextError::err("invalid RTU connection params"));
-        return;
-      }
-      modbus = modbus_new_rtu(
-        connectionPath.c_str(),
-        baudRate,
-        parityEnable ? (evenParity ? 'E' : 'O') : 'N',
-        charSize,
-        twoStopBits ? 2 : 1
-      );
-      if (modbus==0) mberr = errno;
-      if (mberr==0) {
-        if (rs232) {
-          if (modbus_rtu_set_serial_mode(modbus, MODBUS_RTU_RS232)<0) mberr = errno;
-        }
-        else {
-          // set custom RTS if needed (FIRST, otherwise modbus_rtu_set_serial_mode() might fail when TIOCSRS485 does not work)
-          if (mberr==0 && modbusRTSEnable) {
-            if (modbus_rtu_set_custom_rts(modbus, setRts)<0) mberr = errno;
-          }
-          if (mberr==0) {
-            if (modbus_rtu_set_serial_mode(modbus, MODBUS_RTU_RS485)<0) mberr = errno;
-          }
-          if (mberr==0) {
-            if (modbus_rtu_set_rts(modbus, MODBUS_RTU_RTS_UP)<0) mberr = errno;
-          }
-        }
-        if (mberr==0) {
-          int rtsDelayUs;
-          if (getIntOption("rs485rtsdelay", rtsDelayUs)) {
-            if (modbus_rtu_set_rts_delay(modbus, rtsDelayUs)<0) mberr = errno;
-          }
-        }
-      }
-    }
-    else {
-      if (connectionPath.empty()) {
-        terminateAppWith(TextError::err("invalid TCP connection params"));
-        return;
-      }
-      modbus = modbus_new_tcp(connectionPath.c_str(), connectionPort);
-      if (modbus==0) mberr = errno;
-    }
-    if (mberr!=0) {
-      terminateAppWith(ModBusError::err<ModBusError>(mberr));
-      return;
-    }
-    getIntOption("slave", slave);
-    LOG(LOG_NOTICE, "successfully initialized modbus");
-  }
-
-
-  ErrorPtr mb_read_registers(int slave, int addr, int nb, uint16_t *dest)
-  {
-    ErrorPtr err;
-    if (modbus_set_slave(modbus, slave)<0) {
-      err = ModBusError::err<ModBusError>(errno);
-    }
-    else {
-      if (modbus_connect(modbus)<0) {
-        err = ModBusError::err<ModBusError>(errno);
-      }
-      else {
-        if (modbus_read_registers(modbus, addr, nb, dest)<0) {
-          err = ModBusError::err<ModBusError>(errno);
-        }
-      }
-    }
-    modbus_close(modbus);
-    return err;
-  }
-
-
-  ErrorPtr mb_write_registers(int slave, int addr, int nb, uint16_t *src)
-  {
-    ErrorPtr err;
-    if (modbus_set_slave(modbus, slave)<0) {
-      err = ModBusError::err<ModBusError>(errno);
-    }
-    else {
-      if (modbus_connect(modbus)<0) {
-        err = ModBusError::err<ModBusError>(errno);
-      }
-      else {
-        if (modbus_write_registers(modbus, addr, nb, src)<0) {
-          err = ModBusError::err<ModBusError>(errno);
-        }
-      }
-    }
-    modbus_close(modbus);
-    return err;
-  }
-
 
 
 
@@ -524,11 +378,11 @@ public:
   {
     testmode = getOption("testmode");
     // get status
-    mb_read_registers(slave, 104, 1, &reg104);
-    mb_read_registers(slave, 202, 1, &reg202);
+    modBus.readRegister(104, reg104);
+    modBus.readRegister(202, reg202);
     // start in normal mode
     reg137 = 0;
-    mb_write_registers(slave, 137, 1, &reg137);
+    modBus.writeRegister(137, reg137);
     // repeatedly
     appTicket.executeOnce(boost::bind(&P44mbcd::appTask, this, _1, _2));
   }
@@ -538,29 +392,29 @@ public:
 
   void appTask(MLTimer &aTimer, MLMicroSeconds aNow)
   {
-    ErrorPtr err = mb_read_registers(slave, 104, 1, &reg104);
+    ErrorPtr err = modBus.readRegister(104, reg104);
     if (Error::isOK(err)) {
       if (testmode) {
         if (reg104==1 || reg104==2) {
           // heating
           if (reg137!=1) {
             reg137 = 1; // heating test
-            mb_write_registers(slave, 137, 1, &reg137);
+            modBus.writeRegister(137, reg137);
           }
         }
         else if (reg104==3 || reg104==4) {
           // cooling
           if (reg137!=2) {
             reg137 = 2; // cooling test
-            mb_write_registers(slave, 137, 1, &reg137);
+            modBus.writeRegister(137, reg137);
           }
         }
         else if (reg104==0) {
           reg137 = 0; // off
-          mb_write_registers(slave, 137, 1, &reg137);
+          modBus.writeRegister(137, reg137);
         }
       }
-      mb_read_registers(slave, 202, 1, &reg202);
+      modBus.readRegister(202, reg202);
       update_display();
     }
     else {
@@ -591,14 +445,14 @@ public:
       if (reg104<4) {
         reg104++;
       }
-      mb_write_registers(slave, 104, 1, &reg104);
+      modBus.writeRegister(104, reg104);
       update_display();
     }
     if (aButtonId==2) {
       if (reg104>0) {
         reg104--;
       }
-      mb_write_registers(slave, 104, 1, &reg104);
+      modBus.writeRegister(104, reg104);
       update_display();
     }
   }
@@ -695,20 +549,6 @@ public:
 
 
 };
-
-
-extern "C" {
-
-  void setRts(modbus_t *ctx, int on) {
-    P44mbcd* app = dynamic_cast<P44mbcd*>(Application::sharedApplication());
-    if (app) {
-      if (app->modbusRTSEnable) app->modbusRTSEnable->set(on);
-    }
-  }
-
-}
-
-
 
 
 int main(int argc, char **argv)
